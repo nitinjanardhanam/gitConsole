25e8469a1920c31123fd077268b11af07362f408
Nitin Janardhanam <nitinjanardhanam@gmail.com>
2021-05-06 16:28:33 -0400

    simple console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nitin Janardhanam");
+        }
+    }
+}
